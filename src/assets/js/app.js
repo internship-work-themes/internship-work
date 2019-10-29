9107209b1627f092658b0b343a47ac4bbda23ec8
Johann Teske <johann.teske@digitalmobil.com>
2019-10-29 14:29:37 +0100

    adding classchange with scroll function

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,3 +9,19 @@ window.$ = $;
 import './lib/foundation-explicit-pieces';
 
 $(document).foundation();
+
+/**
+ * Collapsing Nav-Bar when scrolling
+ */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("mobile-menu").className = "site-navigation top-bar scrolled-down";
+    document.getElementById("logo-desktop").className = "site-logo scrolled-down";
+
+  } else {
+    document.getElementById("mobile-menu").className = "site-navigation top-bar scrolled-up";
+    document.getElementById("logo-desktop").className = "site-logo scrolled-up";
+  }
+}

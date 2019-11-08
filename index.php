b69dfe14510decbbfabda826969e606093a5770e
Johann Teske <johann.teske@digitalmobil.com>
2019-11-08 13:32:05 +0100

    centering with align-center and changing structure

--- index.php
+++ index.php
@@ -16,43 +16,48 @@
 
 get_header(); ?>
 
-<div class="main-container cell small-10">
-	<div class="main-content-grid grid-x grid-padding-x">
+<div class="content-container cell xxlarge-10">
+	<div class="grid-x">
 
-		<div class="cell medium-9">
-			<main class="main-content">
-				<?php if (have_posts()) : ?>
+		<div class="content-grid-container cell small-10">
+			<div class="content-grid grid-x grid-padding-x">
 
-					<?php /* Start the Loop */ ?>
-					<?php while (have_posts()) : the_post(); ?>
-						<?php get_template_part('template-parts/content', get_post_format()); ?>
-					<?php endwhile; ?>
+				<main class="content cell medium-9">
+					<?php if (have_posts()) : ?>
 
-				<?php else : ?>
-					<?php get_template_part('template-parts/content', 'none'); ?>
+						<?php /* Start the Loop */ ?>
+						<?php while (have_posts()) : the_post(); ?>
+							<?php get_template_part('template-parts/content', get_post_format()); ?>
+						<?php endwhile; ?>
 
-				<?php endif; // End have_posts() check. 
-				?>
+					<?php else : ?>
+						<?php get_template_part('template-parts/content', 'none'); ?>
 
-				<?php /* Display navigation to next/previous pages when applicable */ ?>
-				<?php
-				if (function_exists('foundationpress_pagination')) :
-					foundationpress_pagination();
-				elseif (is_paged()) :
+					<?php endif; // End have_posts() check. 
 					?>
-					<nav id="post-nav">
-						<div class="post-previous"><?php next_posts_link(__('&larr; Older posts', 'foundationpress')); ?></div>
-						<div class="post-next"><?php previous_posts_link(__('Newer posts &rarr;', 'foundationpress')); ?></div>
-					</nav>
-				<?php endif; ?>
 
-			</main>
+					<?php /* Display navigation to next/previous pages when applicable */ ?>
+					<?php
+					if (function_exists('foundationpress_pagination')) :
+						foundationpress_pagination();
+					elseif (is_paged()) :
+						?>
+						<nav id="post-nav">
+							<div class="post-previous"><?php next_posts_link(__('&larr; Older posts', 'foundationpress')); ?></div>
+							<div class="post-next"><?php previous_posts_link(__('Newer posts &rarr;', 'foundationpress')); ?></div>
+						</nav>
+					<?php endif; ?>
+
+				</main>
+
+				<div class="sidebar-area cell medium-3">
+					<?php get_sidebar(); ?>
+				</div>
+
+			</div>
 		</div>
 
 
-		<div class="cell medium-3">
-			<?php get_sidebar(); ?>
-		</div>
 
 	</div>
 </div>
